1c7a2f6a9690fdd150d5dfc40e75c42c6c4b5097
NickBurrows <nick.github1@gmail.com>
2024-01-21 15:53:52 +0800

    npm init

---
+++ index.js
@@ -0,0 +1,37 @@
+const functions = require('@google-cloud/functions-framework');
+const escapeHtml = require('escape-html');
+const line = require("@line/bot-sdk");
+
+const config = {
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+const client = new line.messagingApi.MessagingApiClient(config);
+
+functions.http('helloHttp', (req, res) => {
+  res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
+});
+
+functions.http('webhook', (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then(result => res.json(result))
+    .catch(err => {
+      log(`ERROR`, err);
+      res.status(err.status || 500).send(err.mesage)
+      res.status(500).end();
+    });
+});
+
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: "text", text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
