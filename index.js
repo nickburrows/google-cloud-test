6a5162d93255c5074fb1ce36ae6d39c6e1c49df8
NickBurrows <nick.github1@gmail.com>
2024-01-28 02:46:00 +0800

    新增數則Flex訊息內容

--- index.js
+++ index.js
@@ -218,135 +218,17 @@ function handleText(message, replyToken, source) {
         ],
       });
     case "image carousel":
-      return client.replyMessage({
-        replyToken,
-        messages: [
-          {
-            type: "template",
-            altText: "Image carousel alt text",
-            template: {
-              type: "image_carousel",
-              columns: [
-                {
-                  imageUrl: buttonsImageURL,
-                  action: {
-                    label: "Go to LINE",
-                    type: "uri",
-                    uri: "https://line.me",
-                  },
-                },
-                {
-                  imageUrl: buttonsImageURL,
-                  action: {
-                    label: "Say hello1",
-                    type: "postback",
-                    data: "hello こんにちは",
-                  },
-                },
-                {
-                  imageUrl: buttonsImageURL,
-                  action: {
-                    label: "Say message",
-                    type: "message",
-                    text: "Rice=米",
-                  },
-                },
-                {
-                  imageUrl: buttonsImageURL,
-                  action: {
-                    label: "datetime",
-                    type: "datetimepicker",
-                    data: "DATETIME",
-                    mode: "datetime",
-                  },
-                },
-              ],
-            },
-          },
-        ],
-      });
+      return client.replyMessage(
+        imageCarouselMessage(replyToken, buttonsImageURL)
+      );
+    case "flex":
+      return client.replyMessage(flexMessage(replyToken));
+    case "menu":
+      return client.replyMessage(menuMessage(replyToken));
     case "datetime":
-      return client.replyMessage({
-        replyToken,
-        messages: [
-          {
-            type: "template",
-            altText: "Datetime pickers alt text",
-            template: {
-              type: "buttons",
-              text: "Select date / time !",
-              actions: [
-                {
-                  type: "datetimepicker",
-                  label: "date",
-                  data: "DATE",
-                  mode: "date",
-                },
-                {
-                  type: "datetimepicker",
-                  label: "time",
-                  data: "TIME",
-                  mode: "time",
-                },
-                {
-                  type: "datetimepicker",
-                  label: "datetime",
-                  data: "DATETIME",
-                  mode: "datetime",
-                },
-              ],
-            },
-          },
-        ],
-      });
+      return client.replyMessage(datetimeMessage(replyToken));
     case "imagemap":
-      return client.replyMessage({
-        replyToken,
-        messages: [
-          {
-            type: "imagemap",
-            baseUrl: `${baseURL}/static/rich`,
-            altText: "Imagemap alt text",
-            baseSize: { width: 1040, height: 1040 },
-            actions: [
-              {
-                area: { x: 0, y: 0, width: 520, height: 520 },
-                type: "uri",
-                linkUri: "https://store.line.me/family/manga/en",
-              },
-              {
-                area: { x: 520, y: 0, width: 520, height: 520 },
-                type: "uri",
-                linkUri: "https://store.line.me/family/music/en",
-              },
-              {
-                area: { x: 0, y: 520, width: 520, height: 520 },
-                type: "uri",
-                linkUri: "https://store.line.me/family/play/en",
-              },
-              {
-                area: { x: 520, y: 520, width: 520, height: 520 },
-                type: "message",
-                text: "URANAI!",
-              },
-            ],
-            video: {
-              originalContentUrl: `${baseURL}/static/imagemap/video.mp4`,
-              previewImageUrl: `${baseURL}/static/imagemap/preview.jpg`,
-              area: {
-                x: 280,
-                y: 385,
-                width: 480,
-                height: 270,
-              },
-              externalLink: {
-                linkUri: "https://line.me",
-                label: "LINE",
-              },
-            },
-          },
-        ],
-      });
+      return client.replyMessage(imagemapMessage(replyToken));
     case "bye":
       switch (source.type) {
         case "user":
@@ -364,6 +246,352 @@ function handleText(message, replyToken, source) {
       console.log(`Echo message to ${replyToken}: ${message.text}`);
       return replyText(replyToken, message.text);
   }
+}
+
+function imagemapMessage(replyToken) {
+  return {
+    replyToken,
+    messages: [
+      {
+        type: "imagemap",
+        baseUrl: `${baseURL}/static/rich`,
+        altText: "Imagemap alt text",
+        baseSize: { width: 1040, height: 1040 },
+        actions: [
+          {
+            area: { x: 0, y: 0, width: 520, height: 520 },
+            type: "uri",
+            linkUri: "https://store.line.me/family/manga/en",
+          },
+          {
+            area: { x: 520, y: 0, width: 520, height: 520 },
+            type: "uri",
+            linkUri: "https://store.line.me/family/music/en",
+          },
+          {
+            area: { x: 0, y: 520, width: 520, height: 520 },
+            type: "uri",
+            linkUri: "https://store.line.me/family/play/en",
+          },
+          {
+            area: { x: 520, y: 520, width: 520, height: 520 },
+            type: "message",
+            text: "URANAI!",
+          },
+        ],
+        video: {
+          originalContentUrl: `${baseURL}/static/imagemap/video.mp4`,
+          previewImageUrl: `${baseURL}/static/imagemap/preview.jpg`,
+          area: {
+            x: 280,
+            y: 385,
+            width: 480,
+            height: 270,
+          },
+          externalLink: {
+            linkUri: "https://line.me",
+            label: "LINE",
+          },
+        },
+      },
+    ],
+  };
+}
+
+function menuMessage(replyToken) {
+  return {
+    replyToken,
+    messages: [
+      {
+        "type": "flex",
+        "altText": "a menu Flex Message",
+        "contents": {
+          "type": "bubble",
+          "hero": {
+            "type": "image",
+            "url":
+              "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_2_restaurant.png",
+            "size": "full",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover",
+            "action": {
+              "type": "uri",
+              "uri": "https://linecorp.com",
+            },
+          },
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "md",
+            "action": {
+              "type": "uri",
+              "uri": "https://linecorp.com",
+            },
+            "contents": [
+              {
+                "type": "text",
+                "text": "Brown's Burger",
+                "size": "xl",
+                "weight": "bold",
+              },
+              {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                      {
+                        "type": "icon",
+                        "url":
+                          "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_regular_32.png",
+                      },
+                      {
+                        "type": "text",
+                        "text": "$10.5",
+                        "weight": "bold",
+                        "margin": "sm",
+                        "flex": 0,
+                      },
+                      {
+                        "type": "text",
+                        "text": "400kcl",
+                        "size": "sm",
+                        "align": "end",
+                        "color": "#aaaaaa",
+                      },
+                    ],
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                      {
+                        "type": "icon",
+                        "url":
+                          "https://scdn.line-apps.com/n/channel_devcenter/img/fx/restaurant_large_32.png",
+                      },
+                      {
+                        "type": "text",
+                        "text": "$15.5",
+                        "weight": "bold",
+                        "margin": "sm",
+                        "flex": 0,
+                      },
+                      {
+                        "type": "text",
+                        "text": "550kcl",
+                        "size": "sm",
+                        "align": "end",
+                        "color": "#aaaaaa",
+                      },
+                    ],
+                  },
+                ],
+              },
+              {
+                "type": "text",
+                "text": "Sauce, Onions, Pickles, Lettuce & Cheese",
+                "wrap": true,
+                "color": "#aaaaaa",
+                "size": "xxs",
+              },
+            ],
+          },
+          "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "button",
+                "style": "primary",
+                "color": "#905c44",
+                "margin": "xxl",
+                "action": {
+                  "type": "uri",
+                  "label": "Add to Cart",
+                  "uri": "https://linecorp.com",
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
+}
+
+function flexMessage(replyToken) {
+  return {
+    replyToken,
+    messages: [
+      {
+        "type": "flex",
+        "altText": "This is a Flex Message",
+        "contents": {
+          "type": "bubble",
+          "header": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Flex Message Corp",
+                "color": "#FFFFFF",
+                "weight": "bold",
+              },
+            ],
+          },
+          "hero": {
+            "type": "image",
+            "url":
+              "https://developers.line.biz/media/messaging-api/using-flex-message-simulator/mary.png",
+            "size": "xl",
+          },
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Mary Smith",
+                "size": "xl",
+                "weight": "bold",
+                "align": "center",
+              },
+              {
+                "type": "text",
+                "text": "Founder at Flex Message Corp",
+                "align": "center",
+              },
+              {
+                "type": "separator",
+                "margin": "md",
+              },
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Visit our website",
+                      "uri": "https://example.com",
+                    },
+                    "style": "link",
+                  },
+                  {
+                    "type": "button",
+                    "action": {
+                      "type": "uri",
+                      "label": "Register with us",
+                      "uri": "https://liff.line.me/xxxxx-xxxxx/",
+                    },
+                    "style": "link",
+                  },
+                ],
+                "paddingTop": "10px",
+              },
+            ],
+          },
+          "styles": {
+            "header": {
+              "backgroundColor": "#00B900",
+            },
+          },
+        },
+      },
+    ],
+  };
+}
+
+function datetimeMessage(replyToken) {
+  return {
+    replyToken,
+    messages: [
+      {
+        type: "template",
+        altText: "Datetime pickers alt text",
+        template: {
+          type: "buttons",
+          text: "Select date / time !",
+          actions: [
+            {
+              type: "datetimepicker",
+              label: "date",
+              data: "DATE",
+              mode: "date",
+            },
+            {
+              type: "datetimepicker",
+              label: "time",
+              data: "TIME",
+              mode: "time",
+            },
+            {
+              type: "datetimepicker",
+              label: "datetime",
+              data: "DATETIME",
+              mode: "datetime",
+            },
+          ],
+        },
+      },
+    ],
+  };
+}
+
+function imageCarouselMessage(replyToken, buttonsImageURL) {
+  return {
+    replyToken,
+    messages: [
+      {
+        type: "template",
+        altText: "Image carousel alt text",
+        template: {
+          type: "image_carousel",
+          columns: [
+            {
+              imageUrl: buttonsImageURL,
+              action: {
+                label: "Go to LINE",
+                type: "uri",
+                uri: "https://line.me",
+              },
+            },
+            {
+              imageUrl: buttonsImageURL,
+              action: {
+                label: "Say hello1",
+                type: "postback",
+                data: "hello こんにちは",
+              },
+            },
+            {
+              imageUrl: buttonsImageURL,
+              action: {
+                label: "Say message",
+                type: "message",
+                text: "Rice=米",
+              },
+            },
+            {
+              imageUrl: buttonsImageURL,
+              action: {
+                label: "datetime",
+                type: "datetimepicker",
+                data: "DATETIME",
+                mode: "datetime",
+              },
+            },
+          ],
+        },
+      },
+    ],
+  };
 }
 
 async function handleImage(message, replyToken) {
